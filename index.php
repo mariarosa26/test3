4db263e8d4275dcf46a2cd1282729510ded8b31a
mariarosa26 <acutjohndominic5@gmail.com>
2024-11-14 10:47:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 <body>
- <p> My first ubuntu server PHP Deployment</p>
-</body>
+  <form action="signup.php" method="POST">
 
 <div>
 	<label>Firstname:</label>
@@ -42,6 +41,8 @@
 	<div>
 	<button type="submit" class="btn btn-primary">Submit</button>
 </div> 
+</body>
+
 </html>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
